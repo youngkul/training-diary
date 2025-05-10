ad8947a8e9a71501df1ba23ee54e1e960c29c7d2
young kul yang <youngkulyang@youngui-MacBookPro.local>
2025-05-10 17:12:09 +0900

    🔥 fix: firebase-config.js 강제 포함

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// firebase-config.js
+import { firebaseConfig } from "./env.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+
+
+
+// force deploy
